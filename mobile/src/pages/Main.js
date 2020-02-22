1781ebfb625bd642df7166a7d72b21ecb9d85fcb
LucasrsRodrigues <lucasrs.rodrigues3@outlook.com>
2020-02-22 14:56:09 -0300

    mobile

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -9,21 +9,28 @@ function Main(){
   
   useEffect(() => {
     async function loadInitialPosition(){
+
       const { granted } = await requestPermissionsAsync();
+
       if(granted){
-        const { coords } = await getCurrentPositionAsync({
+
+        const {coords} = await getCurrentPositionAsync({
           enableHighAccuracy: true,
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta:0.04,
+          longitudeDelta: 0.04,
         });
       }
 
-      const { latitude, longitude } = coords;
+      
 
-      setCurrentRegion({
-        latitude,
-        longitude,
-        latitudeDelta:0.04,
-        longitudeDelta: 0.04,
-      });
+      
 
     }
     loadInitialPosition();
